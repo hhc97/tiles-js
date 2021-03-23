817e778eb23ff727bfa6eb860e8941d159d390d5
Haocheng Hu <hc.hu@utoronto.ca>
2021-03-23 16:02:47 -0400

    add sorting demo

--- pub/js/examples.js
+++ pub/js/examples.js
@@ -1,12 +1,6 @@
+"use strict"
+
 const lib = new TileConstructor()
-
-
-const button = $('#add_demo1')
-
-button[0].addEventListener("click", function () {
-    lib.addTile(d1, 'new tile')
-})
-
 
 
 const d1 = 'demo1'
@@ -22,3 +16,22 @@ lib.addTile(d2, 'Orange', 'https://www.quanta.org/thumbs/thumb-orange-640x480-or
 lib.addTile(d2, 'Apple', 'https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?w=641&ssl=1')
 lib.addTile(d2, 'Lychee', 'https://images-prod.healthline.com/hlcmsresource/images/AN_images/lychees-1296x728-feature.jpg')
 
+const d3 = 'demo3'
+lib.initCanvas(d3)
+for (let i = 0; i < 5; i++) {
+    lib.addTile(d3, i)
+}
+
+// adding listeners to demo buttons
+const button = $('#add_demo1')
+button[0].addEventListener("click", function () {
+    lib.addTile(d1, 'new tile')
+})
+const shuffle = $('#shuffle')
+shuffle[0].addEventListener("click", function () {
+    lib.shuffle(d3)
+})
+const sort = $('#sort')
+sort[0].addEventListener("click", function () {
+    lib.sort(d3)
+})
--- pub/js/tiles.js
+++ pub/js/tiles.js
@@ -8,6 +8,7 @@ class TileConstructor {
         this.container = ''
     }
 
+    // stores the added tile into the tiles array and returns the tile ID
     _getTileID() {
         this.tiles.push({
             id: this.curr_tile,
@@ -19,6 +20,50 @@ class TileConstructor {
         return this.curr_tile - 1
     }
 
+    // a helper to shuffle arrays in place
+    _shuffleArrayInPlace(a) {
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a
+    }
+
+    // sorts the tiles in the container by their ID
+    sort(container) {
+        let toSort = document.getElementById(container).children
+        toSort = Array.prototype.slice.call(toSort, 0)
+
+        toSort.sort(function (a, b) {
+            let aord = parseInt(a.id)
+            let bord = parseInt(b.id)
+            return (aord > bord) ? 1 : -1
+        })
+
+        let parent = document.getElementById(container);
+        parent.innerHTML = ""
+
+        for (let i = 0, l = toSort.length; i < l; i++) {
+            parent.appendChild(toSort[i])
+        }
+    }
+
+    // shuffles the tiles in the container by their ID
+    shuffle(container) {
+        let toSort = document.getElementById(container).children
+        toSort = Array.prototype.slice.call(toSort, 0)
+
+        this._shuffleArrayInPlace(toSort)
+
+        let parent = document.getElementById(container)
+        parent.innerHTML = ""
+
+        for (let i = 0, l = toSort.length; i < l; i++) {
+            parent.appendChild(toSort[i])
+        }
+    }
+
+    // initializes the canvas div
     initCanvas(container) {
         const canvas = $('#' + container)
         this.container = container
@@ -30,7 +75,7 @@ class TileConstructor {
         canvas.css('grid-auto-rows', '100px')
     }
 
-
+    // adds a tile to the canvas
     addTile(container, title = 'Title', img_src = '') {
         const canvas = $('#' + container)
 
