ef9e81868c1f99432925d3c6646103b12e2909a3
Haocheng Hu <hc.hu@utoronto.ca>
2021-03-23 01:02:22 -0400

    add images

--- examples.js
+++ examples.js
@@ -7,3 +7,10 @@ lib.addTile(d1, 'Here')
 lib.addTile(d1, 'are')
 lib.addTile(d1, 'some')
 lib.addTile(d1, 'tiles.')
+
+const d2 = 'demo2'
+lib.initCanvas(d2)
+lib.addTile(d2, 'Orange' , 'https://www.quanta.org/thumbs/thumb-orange-640x480-orange.jpg')
+lib.addTile(d2, 'Apple', 'https://i2.wp.com/ceklog.kindel.com/wp-content/uploads/2013/02/firefox_2018-07-10_07-50-11.png?w=641&ssl=1')
+lib.addTile(d2, 'Lychee', 'https://images-prod.healthline.com/hlcmsresource/images/AN_images/lychees-1296x728-feature.jpg')
+
--- tiles.js
+++ tiles.js
@@ -1,3 +1,5 @@
+"use strict"
+
 // the class that contains the library and all its functions
 class TileConstructor {
     constructor() {
@@ -15,7 +17,7 @@ class TileConstructor {
         canvas.css('grid-template-rows', '100px')
     }
 
-    addTile(container, title = 'Title') {
+    addTile(container, title = 'Title', img_src = '') {
         const canvas = $('#' + container)
 
         const tile = document.createElement('div')
@@ -24,6 +26,15 @@ class TileConstructor {
         tile.style.borderRadius = '5px'
         tile.style.boxShadow = '0 0 3pt 2pt black'
         tile.style.textAlign = 'center'
+
+        if (img_src !== '') {
+            const img = document.createElement('img')
+            img.src = img_src
+            img.style.height = '80%'
+            img.style.width = '100%'
+            tile.append(img)
+        }
+
         canvas.append(tile)
     }
 }
