ec06aabda0dae683d32798a3fef5c79cf02b69fd
Haocheng Hu <hc.hu@utoronto.ca>
2021-04-17 01:02:31 -0400

    add tiny tile animations

--- pub/js/tiles.js
+++ pub/js/tiles.js
@@ -35,6 +35,29 @@
         }
         return a
     }
+
+    // helper to animate tile size changes
+    function _animateChange(id, startW, startH, endW, endH) {
+        let elem = document.getElementById(id);
+        let w = startW
+        let h = startH
+        let animateID = setInterval(animate, 5);
+        function animate() {
+            if (w === endW && h === endH) {
+                clearInterval(animateID);
+            } else {
+                w < endW ? w++ : w === endW || w--
+                h < endH ? h++ : h === endH || h--
+                elem.style.width = w + "px";
+                elem.style.height = h + "px";
+            }
+        }
+    }
+
+    // helper for getting a short delay in code execution
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
     /* End of private properties/functions */
 
     TileConstructor.prototype = {
@@ -85,13 +108,20 @@
             tile.style.borderRadius = '5px'
             tile.style.boxShadow = '0 0 3pt 2pt black'
             tile.style.textAlign = 'center'
-            tile.onmouseenter = function (event) {
+            tile.onmouseenter = (event) => {
                 const target = event.target
                 target.style.boxShadow = `0 0 3pt 2pt ${hover_color}`
+                target.style.zIndex = 2
+                _animateChange(target.id, this.width, this.height,
+                    Math.ceil(1.2 * this.width), Math.ceil(1.2 * this.height))
             }
-            tile.onmouseleave = function (event) {
+            tile.onmouseleave = async (event) => {
                 const target = event.target
                 target.style.boxShadow = '0 0 3pt 2pt black'
+                target.style.zIndex = 1
+                await sleep(10)
+                _animateChange(target.id, Math.ceil(1.2 * this.width),
+                    Math.ceil(1.2 * this.height), this.width, this.height)
             }
             tile.onclick = function (event) {
                 alert('tile clicked (functionality not added yet)')
