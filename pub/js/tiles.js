786387c09e6074a9af6f70340ce704161b5eb923
Haocheng Hu <hc.hu@utoronto.ca>
2021-04-17 02:25:41 -0400

    add drag and drop for tiles

--- pub/js/tiles.js
+++ pub/js/tiles.js
@@ -56,6 +56,37 @@
         }
     }
 
+    // functions to help with drag and drop functionality
+    function drag(ev) {
+        ev.dataTransfer.setData("dragID", ev.target.id);
+        ev.dataTransfer.setData("dragParent", ev.target.parentElement.id);
+    }
+    function drop(ev) {
+        ev.preventDefault();
+        const dragged = ev.dataTransfer.getData("dragID");
+        const draggedParentID = ev.dataTransfer.getData("dragParent");
+        let child = document.getElementById(ev.target.id)
+        if (child && child.parentElement.id === draggedParentID) {
+            const container = draggedParentID
+            let toSort = document.getElementById(container).children
+            toSort = Array.prototype.slice.call(toSort, 0)
+
+            const first = toSort.findIndex((element) => element.id === dragged)
+            const second = toSort.findIndex((element) => element.id === ev.target.id)
+
+            if (first > -1 && second > -1) {
+                [toSort[first], toSort[second]] = [toSort[second], toSort[first]]
+            }
+
+            let parent = document.getElementById(container)
+            parent.innerHTML = ""
+
+            for (let i = 0, l = toSort.length; i < l; i++) {
+                parent.appendChild(toSort[i])
+            }
+        }
+    }
+
     // helper for getting a short delay in code execution
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
@@ -110,6 +141,7 @@
             tile.style.borderRadius = '5px'
             tile.style.boxShadow = '0 0 3pt 2pt black'
             tile.style.textAlign = 'center'
+            tile.draggable = 'true'
             tile.onmouseenter = (event) => {
                 const target = event.target
                 target.style.boxShadow = `0 0 3pt 2pt ${hover_color}`
@@ -128,12 +160,17 @@
             tile.onclick = function (event) {
                 alert('tile clicked (functionality not added yet)')
             }
+            // add drag and drop functionality
+            tile.ondrop = drop
+            tile.ondragover = (ev) => { ev.preventDefault() }
+            tile.ondragstart = drag
 
             if (img_src !== '') {
                 const img = document.createElement('img')
                 img.src = img_src
                 img.style.height = '80%'
                 img.style.width = '100%'
+                img.style.pointerEvents = 'none'
                 tile.append(img)
             }
             canvas.append(tile)
