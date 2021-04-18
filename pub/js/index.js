a1dba972e5b2409bc1a9b875c1fc27b035c8eae4
Haocheng Hu <hc.hu@utoronto.ca>
2021-04-18 00:21:30 -0400

    add basic landing page

---
+++ pub/js/index.js
@@ -0,0 +1,7 @@
+"use strict"
+
+const lib = new TileConstructor('demo1', 200, 100, true, true)
+
+lib.addTile('', 'static/welcome_word.PNG')
+lib.addTile('', 'static/to_word.PNG')
+lib.addTile('', 'static/tiles_word.PNG')
